dabe7f424cd63dea6da268377306b539d9803c0f
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-11-29 15:14:14 +0530

    object refactor

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,27 @@
-import Part from "./Part"
-
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
-      <Part course={'Half Stack Application development'} part = {'Fundamentals of react'} exercise={'10 exercises'}/>
-      <Part part = {'Using state and props'} exercise={'7 exercises'}/>
-      <Part part = {'State of a component'} exercise={' 14 exercises'}/>
+      <h1>{course}</h1>
+      <h2>Part 1: {part1.name}</h2>
+      <p>Exercises: {part1.exercises}</p>
+      <h2>Part 2: {part2.name}</h2>
+      <p>Exercises: {part2.exercises}</p>
+      <h2>Part 3: {part3.name}</h2>
+      <p>Exercises: {part3.exercises}</p>
     </div>
   )
 }
